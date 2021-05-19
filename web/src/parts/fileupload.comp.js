688141f7d2926927fc04119e2d627c65db0763ae
baiyuqi <1353813001@qq.com>
2021-05-19 10:18:02 +0800

    mm

--- web/src/parts/fileupload.comp.js
+++ web/src/parts/fileupload.comp.js
@@ -1,57 +1,65 @@
-import { Input, FormControl, FormLabel, InputGroup, InputLeftElement, FormErrorMessage, Code, Icon } from "@chakra-ui/react";
+import { Input, FormControl, FormLabel, InputGroup, InputLeftElement, FormErrorMessage, Code, Icon } from "@chakra-ui/react"
+import { atom, useRecoilState } from "recoil"
 
-function previewFile(file) {
-  alert(file)
-  if(file == undefined)
-    return;
-  let reader
-  if (file) {
-    // 创建流对象
-    reader = new FileReader()
-    reader.readAsDataURL(file)
-  }
-  // 捕获 转换完毕
-  reader.onload = function(e) {
-    // 转换后的base64就在e.target.result里面,直接放到img标签的src属性即可
-    document.querySelector('img').src = e.target.result
-  }
-}
+export const pieceState = atom({
+  key: "ART_PIECE",
+  default: null
+})
 
+export const pieceProofState = atom({
+  key: "ART_PIECE_PROOF",
+  default: null
+})
 const FileUpload = () => {
- 
+
+  const [piece, setPiece] = useRecoilState(pieceState);
+  const [pieceProof, setPieceProof] = useRecoilState(pieceProofState);
+  function previewPiece(event) {
+
+    debugger
+
+    let reader = new FileReader()
+    reader.readAsDataURL(event.target.files[0])
 
 
+    reader.onload = function (e) {
+      // 转换后的base64就在e.target.result里面,直接放到img标签的src属性即可
+
+      setPiece(e.target.result)
+    }
+  }
+  function previewPieceProof(event) {
+
+    debugger
+
+    let reader = new FileReader()
+    reader.readAsDataURL(event.target.files[0])
+
+
+    reader.onload = function (e) {
+      // 转换后的base64就在e.target.result里面,直接放到img标签的src属性即可
+
+      setPieceProof(e.target.result)
+    }
+  }
   return (
-    <FormControl   isRequired>
+    <FormControl isRequired>
       <FormLabel htmlFor="writeUpFile"></FormLabel>
       <InputGroup>
-        <InputLeftElement
-          pointerEvents="none"
-         
-        />
-        <Input type='file' accept="image/gif,image/jpeg,image/jpg,image/png,image/svg"  name="dd"  ></Input>
-        <img src="" id="999" alt=""/>
-        <Input
-          placeholder= "你的作品 ..."
-        
-          value=""
-        />
+    
+        <Input type='file' accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" placeholder="你的作品 ..." name="dd" onChange={previewPiece}></Input>
+        <img src={piece} alt=""  height="200" width="200" />
+
       </InputGroup>
       <InputGroup>
         <InputLeftElement
           pointerEvents="none"
-         
         />
-        <Input type='file' accept="image/gif,image/jpeg,image/jpg,image/png,image/svg"  name=""  ></Input>
-        <img src="" id="998" alt=""/>
-        <Input
-          placeholder= "你的授权书 ..."
-        
-          value=""
-        />
+        <Input type='file' accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" name="" onChange={previewPieceProof} ></Input>
+        <img src={pieceProof} alt=""  height="200" width="200" />
       </InputGroup>
       <FormErrorMessage>
-        
+
       </FormErrorMessage>
     </FormControl>
   );
